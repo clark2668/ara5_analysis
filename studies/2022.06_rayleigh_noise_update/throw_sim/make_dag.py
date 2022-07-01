dc26583763d4d0bde507712bcb3a2ef5bd881fa4
Brian Clark <baclark@msu.edu>
2022-07-01 13:19:16 +0900

    update all the code assocaited with throwing simulations

--- studies/2022.06_rayleigh_noise_update/throw_sim/make_dag.py
+++ studies/2022.06_rayleigh_noise_update/throw_sim/make_dag.py
@@ -1,5 +1,9 @@
-setup_file = "/home/brianclark/ARA/DevAraRoot/throw_sim/noise_setup.txt"
-output_dir = "/data/user/brianclark/ARA/ara5_analysis/simulations/noise_tuning/pre_tuning"
+# setup_file = "/home/brianclark/ARA/DevAraRoot/throw_sim/noise_setup.txt"
+# output_dir = "/data/user/brianclark/ARA/ara5_analysis/simulations/noise_tuning/pre_tuning"
+
+setup_file = "/home/brian/ARA/ara5_analysis/ara5_analysis/studies/2022.06_rayleigh_noise_update/throw_sim/noise_setup_1024.txt"
+output_dir = "/disk20/users/brian/ARA/sim"
+
 
 dag_file_name = 'dag_sim.dag'
 instructions = ""
@@ -9,8 +13,8 @@ with open(dag_file_name, 'w') as f:
     f.write(instructions)
 
 
-master_index = 200000
-for run_number in range(0, 20, 1):
+master_index = 40000
+for run_number in range(0, 1, 1):
     instructions = ""
     instructions += f"JOB job_{run_number} run_sim.htc \n"
     instructions += f'VARS job_{run_number} runnumber="{master_index+run_number}" \n\n'
--- studies/2022.06_rayleigh_noise_update/throw_sim/run_sim.sh
+++ studies/2022.06_rayleigh_noise_update/throw_sim/run_sim.sh
@@ -4,10 +4,13 @@ setupfile=$1
 outputdir=$2
 runnumber=$3
 
-source /home/brianclark/ARA/DevAraRoot/setup.sh
+#source /home/brianclark/ARA/DevAraRoot/setup.sh
+source /home/brian/ARA/ara5_analysis/setup.sh
+
 
 # get to the right folder and run AraSim
-cd /home/brianclark/ARA/DevAraRoot/AraSim
+#cd /home/brianclark/ARA/DevAraRoot/AraSim
+cd /home/brian/ARA/ara5_analysis/tools/AraSim
 ./AraSim ${setupfile} ${runnumber} $TMPDIR
 
 # at the end, move the results back
